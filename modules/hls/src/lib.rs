a66418d904f05d4756e9be80eb62623541f7bef9
ThinhPhung <hungthinhphung1996@gmail.com>
2022-09-12 17:59:35 +0700

    Add basic http

--- modules/hls/src/lib.rs
+++ modules/hls/src/lib.rs
@@ -1,3 +1,11 @@
+use bytes::Bytes;
+
+pub fn send_request(url: &str) -> Bytes {
+    let response = reqwest::blocking::get(url).expect(&format!("Send request failed: {}", url)[..]);
+    let data = response.bytes().expect(&format!("Unpack data failed: {}", url)[..]);
+    return data;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use hls::send_request;
 fn main() {
-    println!("Hello, world!");
+    let url = "https://videos-fms.jwpsrv.com/631fb3b6_0xc4c33e4ac3f312a591a1d147b14d8276f4cfa56b/content/conversions/LOPLPiDX/videos/IFBsp7yL-24721145.mp4-1.ts";
+    send_request(url);
 }

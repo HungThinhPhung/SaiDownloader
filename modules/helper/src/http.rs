b16f3a3e1b0e4fcd6125d307445e4b0cbeabbda4
ThinhPhung <hungthinhphung1996@gmail.com>
2022-09-15 16:13:49 +0700

    Remove erratic variable

--- modules/helper/src/http.rs
+++ modules/helper/src/http.rs
@@ -15,10 +15,8 @@ pub fn lines_to_header(lines: &Vec<String>) -> HeaderMap {
         // Skip no colon line
         if !(split.len() == 2) { continue };
 
-        let owned_name = split[0].to_owned();
-        let owned_value = split[1].to_owned();
-        let header_name = HeaderName::from_str(owned_name.as_ref()).unwrap();
-        let header_value = HeaderValue::from_str(owned_value.as_ref()).unwrap();
+        let header_name = HeaderName::from_str(split[0]).unwrap();
+        let header_value = HeaderValue::from_str(split[1]).unwrap();
         headers.append(header_name, header_value);
     }
     return headers;
